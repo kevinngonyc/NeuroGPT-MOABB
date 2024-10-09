d5507485c4302264c03fc1b53a5a76f9fa320c8d
Kevin Ngo <kevinngonyc@gmail.com>
2024-10-09 11:51:02 -0400

    braindecode

--- neurogpt_moabb.py
+++ neurogpt_moabb.py
@@ -1,15 +1,18 @@
 from train_gpt import make_model, get_config
 from sklearn.pipeline import make_pipeline
 from moabb.evaluations import WithinSessionEvaluation
-from moabb.paradigms import MotorImagery
-from moabb.datasets import AlexMI
+from moabb.paradigms import LeftRightImagery
+from moabb.datasets import BNCI2014_004
+from braindecode import EEGClassifier
+from braindecode.preprocessing import preprocess, Preprocessor
 
-model = make_model(get_config())
-model.from_pretrained("pytorch_model.bin")
+pytorch_model = make_model(get_config())
+pytorch_model.from_pretrained("pytorch_model.bin")
+model = EEGClassifier(pytorch_model)
 
 pipelines = {"NeuroGPT": make_pipeline(model)}
-datasets = [AlexMI()]
-paradigm = MotorImagery(n_classes=3)
+datasets = [BNCI2014_004()]
+paradigm = LeftRightImagery()
 
 evaluation = WithinSessionEvaluation(
     paradigm=paradigm, 
